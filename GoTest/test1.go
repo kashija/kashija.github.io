5f64af23f17863e7e81c310108e485ae29928f11
kashija <32795719+kashija@users.noreply.github.com>
2017-10-14 23:11:01 +0900

    Add files via upload

---
+++ GoTest/test1.go
@@ -0,0 +1,10 @@
+package main
+ 
+import "fmt"
+ 
+// 이것은 주석이다
+ 
+func main() {
+    fmt.Println("Hello World")
+
+}
---
+++ GoTest/test2.go
@@ -0,0 +1,13 @@
+package main
+ 
+import "fmt"
+ 
+func main() {
+    fmt.Println(len("Hello World"))
+
+    fmt.Println("Hello World")
+
+    fmt.Println("Hello " + "World")
+
+    fmt.Println("Hello World"[1])
+}
